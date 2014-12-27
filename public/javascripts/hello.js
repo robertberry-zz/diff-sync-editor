69ac5f4ca0180201d63c2726f782dda4564c3926
Robert Berry <robert.berry@theguardian.com>
2014-12-27 20:49:42 +0000

    Add connect script for socket

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -2,12 +2,24 @@ function checksum(message) {
   return CryptoJS.SHA1(message);
 }
 
+function createConnection() {
+  var connection = new WebSocket("/socket");
+
+  connection.onopen = function () {
+    console.log("Connection opened!");
+  };
+
+  connection.onmessage = function (message) {
+    var m = JSON.parse(message.data);
+
+    console.log("Message", m);
+  };
+
+  connection.onerror = function (error) {
+    console.log("Error", error);
+  };
+}
+
 (function () {
-
-
-
-
-
-
-
+  var conn = createConnection();
 })();
